1b201982427831fe7e31d4ce4a4e3ca505d25ec3
Nazmulshuvo123 <nazmulshuvo161199@gmail.com>
2024-09-04 12:10:08 +0600

    added a proparty and value in a object

---
+++ task-2.js
@@ -0,0 +1,20 @@
+/***
+ * For this object below add a property named passenger capacity with value 5
+
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+ */
+
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+const propName = 'passenger';
+car[propName] = 5;
+console.log(car)
+
+
